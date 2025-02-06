e2a25371dc39ffbbc26dd87eaf8dfae95c50ea5a
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2025-02-06 02:52:09 +0100

    fix: now actually parses month/day/year...gdmmit usa

--- test/utils.cpp
+++ test/utils.cpp
@@ -17,4 +17,20 @@ TEST(Timestamp, Mid) {
 
   EXPECT_EQ(t, 965722088);
 }
+
+TEST(Timestamp, Specific) {
+  const std::string str = "1/18/2025 14:09:01.7451";
+
+  const std::time_t t = Timestamp(str);
+
+  EXPECT_EQ(t, 1737209341);
+}
+
+TEST(Timestamp, Midday) {
+  const std::string str = "1/18/2025 00:09:01.7451";
+
+  const std::time_t t = Timestamp(str);
+
+  EXPECT_EQ(t, 1737158941);
+}
 }
